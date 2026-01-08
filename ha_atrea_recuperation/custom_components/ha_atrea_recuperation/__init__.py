10b17e203c2a907b76c72a47ca9ef2cfa8d2cd93
Miroslav Mudrik <miroslav@mudrik.cz>
2026-01-08 12:40:27 +0100

    Fix empty spaces

---
+++ ha_atrea_recuperation/custom_components/ha_atrea_recuperation/__init__.py
@@ -0,0 +1,10 @@
+# ...existing code...
+
+# removed unused import that caused F401
+# (was) from typing import Any
+
+# removed unused import that caused F401
+# (was) from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+# ...existing code...
+
